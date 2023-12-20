e5b322a5a07674caff2a20dc443e7df066754d86
andkstrr <andikasatrionurcahyo@gmail.com>
2023-12-20 19:47:25 +0700

    add looping

---
+++ looping.js
@@ -0,0 +1,23 @@
+for(let i = 0; i <= 50; i++) {
+    console.log(i)
+}
+
+let angka = 0;
+
+while(angka <= 50) {
+    console.log(angka);
+    angka++;
+}
+
+// 50 -> 0
+
+for(let k = 50; k >= 0; k--) {
+    console.log(k)
+}
+
+let num = 50;
+
+while(num >= 0) {
+    console.log(num);
+    num--;
+}
